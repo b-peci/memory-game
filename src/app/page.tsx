1794f8a38110d302921e4689508e76e6f7b517a7
Bekim Peci <b-peci@outlook.com>
2023-06-03 13:25:30 +0200

    Finished game logic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,16 +9,15 @@ import Icon5 from "../../public/Icon-5.png"
 import Icon6 from "../../public/Icon-6.png"
 import Icon7 from "../../public/Icon-7.png"
 import Icon8 from "../../public/Icon-8.png"
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 export default function Home() {
   const [timer, setTimer] = useState(30);
-  const [images, setImages] = useState([Icon1, Icon2, Icon3, Icon4, Icon5, Icon6, Icon7, Icon8]);
+  const [images, setImages] = useState<Array<StaticImageData>>([]);
   const [imageIndexes, setImageIndexes] = useState<Array<number>>([])
-  const [selectedImages, setSelectedImages] = useState<Array<StaticImageData>>([])
-  const [matchedImages, setMatchedImages] = useState<Array<StaticImageData>>([]);
-  const imagesTwice: Map<number, number> = new Map<number, number>;
-  let availableIndexes = [0, 1, 2, 3, 4, 5, 6, 7]
+  const [selectedImages, setSelectedImages] = useState<Array<number>>([])
+  const [indexesToShowImage, setIndexesToShowImages] = useState<Array<number>>([]);
+  const [timeOut, setTimeOut] = useState<any>();
   useEffect(() => {
     if (timer == 0) return;
     const interval = setInterval(() => setTimer(timer - 1), 1000);
@@ -27,36 +26,9 @@ export default function Home() {
   }, [timer]);
 
   useEffect(() => {
-    setImages(shuffle([Icon1, Icon2, Icon3, Icon4, Icon5, Icon6, Icon7, Icon8]))
-    setImageIndexes(shuffle([0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7]))
-    setTimeout(() => {
-      console.log(imageIndexes)
-    }, 4_000)
-    // const array : Array<number> = [];
-    // for(let i =0; i < 16; i++) {
-    //   const index = choseImageSrc();
-    //   array.push(index);
-    // }
-    // console.log(array)
-    // setImageIndexes(array);
+    setImages(shuffle([Icon1, Icon2, Icon3, Icon4, Icon5, Icon6, Icon7, Icon8, Icon1, Icon2, Icon3, Icon4, Icon5, Icon6, Icon7, Icon8]))
+    setImageIndexes(shuffle([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]))
   }, [])
-
-  const hasBeenSelectedOrMatched = (image : StaticImageData): boolean => {
-    return selectedImages.some(x => x.src === image.src)  || matchedImages.some(x => x.src === image.src);
-  }
-
-  const selectImage = (image: StaticImageData) => {
-    debugger;
-    setSelectedImages([...selectedImages, image])
-    if(selectedImages.length == 2) {
-      if(selectedImages[0] == image) {
-        setMatchedImages([...matchedImages, image]);
-      }
-      setTimeout(() => {
-        setSelectedImages([]);
-      }, 3000)
-    }
-  }
 
   const shuffle = (array: Array<any>): Array<any> => {
     let currentIndex = array.length, randomIndex;
@@ -75,69 +47,60 @@ export default function Home() {
 
     return array;
   }
+  const [isTimeoutExecuting, setIsTimeoutExecuting] = useState(false);
+  const onClickEvent = (index: number) => {
+    if(isTimeoutExecuting) return;
+    const indexes = [...indexesToShowImage, index];
+    if(indexes.length % 2 === 1) {
+      setIndexesToShowImages([...indexesToShowImage, index])
+    }
+    else {
+      setIndexesToShowImages([...indexesToShowImage, index])
+      const indexes = [...indexesToShowImage, index];
+      setTimeout(() => {
+        let  hasGoneInsideIf = false;
+        setIsTimeoutExecuting(true);
+        for(let i = 0; i < indexes.length - 1; i++) {
+            if(images[indexes[indexes.length - 1]].src === images[indexes[i]].src) {
+              hasGoneInsideIf = true;
+            }
+        }
+        if(!hasGoneInsideIf) {
+          setIndexesToShowImages(indexesToShowImage.slice(0, -1))
+        }
+        setIsTimeoutExecuting(false);
+      }, 1000)
+    }
+  }
 
   return (
     <div className='flex justify-center h-[100vh]'>
       <div className=' flex flex-col p-3 border w-[42vw] border-solid self-center rounded-xl border-white h-[62vh]'>
         <div className='flex'>
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[1]])
-          }}
-           imageSrc={
-            hasBeenSelectedOrMatched(images[imageIndexes[1]]) ? images[imageIndexes[1]] : ''} />
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[4]])
-          }}
-           imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[4]]) ?  images[imageIndexes[4]] : ''} />
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[5]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[5]]) ?  images[imageIndexes[5]] : ''} />
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[3]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[3]]) ?  images[imageIndexes[3]] : ''} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 0) ? images[0] : ''} onClick={() => onClickEvent(0)} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 1) ? images[1] : ''} onClick={() => onClickEvent(1)} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 2) ? images[2] : ''} onClick={() => onClickEvent(2)} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 3) ? images[3] : ''} onClick={() => onClickEvent(3)} />
         </div>
         <div className='flex'>
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[2]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[2]]) ?  images[imageIndexes[2]] : ''} />
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[0]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[0]]) ?  images[imageIndexes[0]] : ''} />
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[6]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[6]]) ?  images[imageIndexes[6]] : ''} />
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[7]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[7]]) ?  images[imageIndexes[7]] : ''} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 4) ? images[4] : ''} onClick={() => onClickEvent(4)} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 5) ? images[5] : ''} onClick={() => onClickEvent(5)} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 6) ? images[6] : ''} onClick={() => onClickEvent(6)} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 7) ? images[7] : ''} onClick={() => onClickEvent(7)} />
         </div>
         <div className='flex'>
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[8]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[8]]) ?  images[imageIndexes[8]] : ''} />
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[9]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[9]]) ?  images[imageIndexes[9]] : ''} />
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[10]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[10]]) ?  images[imageIndexes[10]] : ''} />
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[11]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[11]]) ?  images[imageIndexes[11]] : ''} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 8) ? images[8] : ''} onClick={() => onClickEvent(8)} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 9) ? images[9] : ''} onClick={() => onClickEvent(9)} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 10) ? images[10] : ''} onClick={() => onClickEvent(10)} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 11) ? images[11] : ''} onClick={() => onClickEvent(11)} />
         </div>
         <div className='flex'>
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[12]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[12]]) ?  images[imageIndexes[12]] : ''} />
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[13]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[13]]) ?  images[imageIndexes[13]] : ''} />
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[14]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[14]]) ?  images[imageIndexes[14]] : ''} />
-          <Box onClick={() => {
-            selectImage(images[imageIndexes[15]])
-          }} imageSrc={hasBeenSelectedOrMatched(images[imageIndexes[15]]) ?  images[imageIndexes[15]] : ''} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 12) ? images[12] : ''} onClick={() => onClickEvent(12)} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 13) ? images[13] : ''} onClick={() => onClickEvent(13)} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 14) ? images[14] : ''} onClick={() => onClickEvent(14)} />
+          <Box imageSrc={indexesToShowImage?.some(x => x === 15) ? images[15] : ''} onClick={() => onClickEvent(15)} />
         </div>
+
         <div className='flex  p-3 justify-around border-solid border-white border'>
           <p className='text-xl'>Time: {timer}s</p>
           <button className='rounded-lg text-white bg-blue-600 w-28 text-lg font-bold'>Retry</button>
--- src/components/box.tsx
+++ src/components/box.tsx
@@ -1,15 +1,15 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import Image, { StaticImageData } from "next/image";
 interface BoxProps {
     imageSrc: string | StaticImageData,
-    onClick: () => void;
+    onClick: (element : any) => void;
 }
 
 const Box = (props : BoxProps) => {
     return (
         <>
-            <div onClick={props.onClick} className="border border-solid border-white h-28 m-2 w-28 rounded-lg">
-                {props.imageSrc && <Image src={props.imageSrc} alt="Icon" />}
+            <div   onClick={props.onClick}  className="border border-solid border-white h-28 m-2 w-28 rounded-lg">
+                {props.imageSrc && <Image  src={props.imageSrc} alt="Icon" />}
             </div>
         </>
     )
